6607b66a34ef9feae348e9de50e5af81e0e6f8e7
sgoh0039 <sgoh0039@student.monash.edu>
2023-12-14 09:25:25 +0800

    Added listener for player joining the server

--- src/main/java/papermc/rebelwars/Rebelwars.java
+++ src/main/java/papermc/rebelwars/Rebelwars.java
@@ -1,11 +1,14 @@
 package papermc.rebelwars;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.logging.Logger;
 
-public final class Rebelwars extends JavaPlugin {
+public final class Rebelwars extends JavaPlugin implements Listener {
 
     @Override
     public @NotNull Logger getLogger() {
@@ -14,12 +17,17 @@ public final class Rebelwars extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("Test start");
+        getLogger().info("Plugin start");
+        getServer().getPluginManager().registerEvents(this, this);
+    }
 
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+        getLogger().info("A player has joined the server");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("Test end");
+        getLogger().info("Plugin end");
     }
 }
