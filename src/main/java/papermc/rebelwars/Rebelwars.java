cd3dfe7b98664186389d6262fc0ab24477e7a1d6
sgoh0039 <sgoh0039@student.monash.edu>
2023-12-14 09:02:51 +0800

    test

--- src/main/java/papermc/rebelwars/Rebelwars.java
+++ src/main/java/papermc/rebelwars/Rebelwars.java
@@ -1,17 +1,25 @@
 package papermc.rebelwars;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.logging.Logger;
 
 public final class Rebelwars extends JavaPlugin {
 
     @Override
+    public @NotNull Logger getLogger() {
+        return super.getLogger();
+    }
+
+    @Override
     public void onEnable() {
-        // Plugin startup logic
+        getLogger().info("Test start");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("Test end");
     }
 }
